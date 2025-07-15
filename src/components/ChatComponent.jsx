53f6428a9d03989e3d4ed3fa29a7ab0544d367df
Ayush <ayush.chahal@lpu.in>
2025-07-15 17:55:03 +0530

    adding backend

--- src/components/ChatComponent.jsx
+++ src/components/ChatComponent.jsx
@@ -29,7 +29,8 @@ const ChatComponent = () => {
     setIsLoading(true);
 
     try {
-      const response = await axios.post("http://localhost:5000/api/ask", {
+      const apiBaseUrl = process.env.REACT_APP_API_URL || "http://localhost:5000";
+      const response = await axios.post(`${apiBaseUrl}/api/ask`, {
         question,
       });
       const aiMessage = {
